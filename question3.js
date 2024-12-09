348636ba0a1b25c0b79b0290a06ae35784084e99
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-09 21:34:00 +0300

    question 3 done

---
+++ question3.js
@@ -0,0 +1,7 @@
+//What are variables in JavaScript, and how do you declare them?
+
+//A variable is a container that stores the data that is assigned
+
+// To declare a variable you can use "let" or "const"
+//"let" = you can change the content of the variable
+//"const" = fixed value - cannot be changed
