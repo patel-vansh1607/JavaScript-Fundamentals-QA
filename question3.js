5f9f02db71c7527dbbf6364e22f8a356cef09f45
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-09 21:43:46 +0300

    edited answer

--- question3.js
+++ question3.js
@@ -3,5 +3,3 @@
 //A variable is a container that stores the data that is assigned
 
 // To declare a variable you can use "let" or "const"
-//"let" = you can change the content of the variable
-//"const" = fixed value - cannot be changed
