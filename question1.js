156a799a82ecd52acb78a4f20f9e9ab0d02805bb
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-09 21:28:15 +0300

    question 1 done

---
+++ question1.js
@@ -0,0 +1,6 @@
+//What is JavaScript, and how does it differ from HTML and CSS?
+
+//HTML and CSS are used to style a website 
+ //CSS styles the appearance of a website
+ //HTML gives us power to create texts, buttons and many other.
+ //While Javascript is used to make a wesbite interactive 
