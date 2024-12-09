3cc6a994bb9f8d9ab69eb51e4dad185ac861eaee
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-09 21:44:56 +0300

    number4 done

---
+++ question4.js
@@ -0,0 +1,6 @@
+//What are the differences between var, let, and const?
+
+
+//"var" = you can declare the variable without knowing its type or value
+//"let" = you can change the content of the variable
+//"const" = fixed value - cannot be changed
