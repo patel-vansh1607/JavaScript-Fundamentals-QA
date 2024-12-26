931f97cb9ccbdc708bc128b905354c8324c2eabd
Vansh Patel <patel.vansh1607@gmail.com>
2024-12-26 21:36:29 +0300

    answered

---
+++ question13.js
@@ -0,0 +1,5 @@
+//List and explain three arithmetic operators in JavaScript.
+
+// + : this adds both the numbers - addition
+// - : this substracts the numbers - subtraction
+// * : this multiplies the numbers - multiplication
---
+++ question14.js
@@ -0,0 +1,14 @@
+//What is the difference between logical operators && and ||?
+
+// "&&" - its an AND operator and returns true if both conditions are true.
+        //e.g
+        let a = true;
+        let b = false;
+
+        console.log(a && b);  // - this will give false because one isnt true
+
+// "||" - its an OR operator and returns true if at least one of the conditions is true.
+        let a = true;
+        let b = false;
+
+        console.log(a|| b);  // - this will give true because atleast one is true
